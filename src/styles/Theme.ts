6ec2b5d268288976194507f42789bbded7d298ee
HarenaFiantso <fiantsorav@gmail.com>
2023-11-05 19:04:31 +0300

    refactor: Reformat the theme file code

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,4 +1,5 @@
-/* Color tokens export */
+import { createTheme } from "@mui/material/styles";
+
 export const colorTokens = {
   grey: {
     0: "#FFFFFF",
@@ -29,78 +30,49 @@ export const colorTokens = {
   },
 };
 
-/* MUI Theme setting */
-export const themeSettings = (mode: "dark" | "light") => {
-  return {
+const themeSettings = (mode: "dark" | "light") => {
+  return createTheme({
     palette: {
       mode: mode,
-      ...(mode === "dark"
-        ? {
-            /* Color palette values for dark mode */
-            primary: {
-              dark: colorTokens.primary[200],
-              main: colorTokens.primary[500],
-              light: colorTokens.primary[800],
-            },
-            neutral: {
-              dark: colorTokens.grey[100],
-              main: colorTokens.grey[200],
-              mediumMain: colorTokens.grey[300],
-              medium: colorTokens.grey[400],
-              light: colorTokens.grey[700],
-            },
-            background: {
-              default: colorTokens.grey[900],
-              alt: colorTokens.grey[800],
-            },
-          }
-        : {
-            /* Color palette values for light mode */
-            primary: {
-              dark: colorTokens.primary[700],
-              main: colorTokens.primary[500],
-              light: colorTokens.primary[50],
-            },
-            neutral: {
-              dark: colorTokens.grey[700],
-              main: colorTokens.grey[500],
-              mediumMain: colorTokens.grey[400],
-              medium: colorTokens.grey[300],
-              light: colorTokens.grey[50],
-            },
-            background: {
-              default: colorTokens.grey[10],
-              alt: colorTokens.grey[0],
-            },
-          }),
+      primary: {
+        main:
+          mode === "dark" ? colorTokens.primary[500] : colorTokens.primary[500],
+        light:
+          mode === "dark" ? colorTokens.primary[800] : colorTokens.primary[50],
+      },
+      text: {
+        primary: mode === "dark" ? colorTokens.grey[0] : colorTokens.grey[1000],
+        secondary:
+          mode === "dark" ? colorTokens.grey[200] : colorTokens.grey[800],
+      },
+      background: {
+        default: mode === "dark" ? colorTokens.grey[900] : colorTokens.grey[0],
+        paper: mode === "dark" ? colorTokens.grey[800] : colorTokens.grey[10],
+      },
     },
     typography: {
       fontFamily: ["Poppins", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 40,
       },
       h2: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 32,
       },
       h3: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 24,
       },
       h4: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 20,
       },
       h5: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 16,
       },
       h6: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 14,
       },
     },
-  };
+  });
 };
+
+export default themeSettings;
