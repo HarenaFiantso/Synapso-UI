df5f0ff31eddf8bad9a05d9849f4674acf74a832
HarenaFiantso <fiantsorav@gmail.com>
2023-11-05 15:07:40 +0300

    feat: Implement the theme provider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Navbar from "./components/Navbar";
-
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./styles/Theme";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { themeSettings } from "./styles/Theme";
 
 export default function App() {
+  const mode = useSelector((state: any) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-    </ThemeProvider>
+    <div className="App">
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </div>
   );
 }
