e25399e261949e0742292ee1c17ef05b8388dad5
HarenaFiantso <fiantsorav@gmail.com>
2023-11-11 09:37:36 +0300

    feat: Initialize UserWidget component

---
+++ src/components/UserWidget.tsx
@@ -0,0 +1,3 @@
+export default function UserWidget() {
+  return <div>User Widget</div>
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
 import NavbarComponent from '../components/NavbarComponent'
+import UserWidget from '../components/UserWidget'
 
 export default function HomePage() {
+  const isNonMobileScreens = useMediaQuery('(min-width:728px)')
+
   return (
     <Box>
       <NavbarComponent />
+      <Box width='100%' padding='2rem 6%' display={isNonMobileScreens ? 'flex' : 'block'} gap='.5rem' justifyContent='space-between'>
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget />
+        </Box>
+      </Box>
     </Box>
   )
 }
