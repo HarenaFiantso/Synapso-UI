eb492846ee3885218fd06cf33a8698b755d43036
HarenaFiantso <fiantsorav@gmail.com>
2023-11-11 09:57:16 +0300

    chore: Implement the main layout of the UserWidget component

--- src/components/UserWidget.tsx
+++ src/components/UserWidget.tsx
@@ -1,3 +1,36 @@
+import FlexBetween from '../styles/FlexBetween'
+import WidgetWrapper from '../styles/WidgetWrapper'
+import { Box, Typography, useTheme } from '@mui/material'
+
 export default function UserWidget() {
-  return <WidgetWrapper></WidgetWrapper>
+  /* Theme configuration */
+  const { palette } = useTheme() as any
+  const dark = palette.neutral.dark
+  const medium = palette.neutral.medium
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap='.5rem' pb='1.1rem'>
+        <FlexBetween gap='1rem'>
+          Ato tokony misy sary
+          <Box>
+            <Typography
+              variant='h4'
+              fontWeight='700'
+              color={dark}
+              sx={{
+                '&:hover': {
+                  color: palette.primary.light,
+                  cursor: 'pointer'
+                }
+              }}
+            >
+              Fiantso Harena
+            </Typography>
+            <Typography color={medium}>69 friends</Typography>
+          </Box>
+        </FlexBetween>
+      </FlexBetween>
+    </WidgetWrapper>
+  )
 }
