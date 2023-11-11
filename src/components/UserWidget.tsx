26480369786412151e5a52192862c763f1572836
HarenaFiantso <fiantsorav@gmail.com>
2023-11-11 10:24:08 +0300

    chore: Implement the second row of the UserWidget component

--- src/components/UserWidget.tsx
+++ src/components/UserWidget.tsx
@@ -2,17 +2,17 @@ import { Box, Divider, Typography, useTheme } from '@mui/material'
 import FlexBetween from '../styles/FlexBetween'
 import WidgetWrapper from '../styles/WidgetWrapper'
 import UserImage from './UserImage'
-import { ManageAccountsOutlined } from '@mui/icons-material'
+import { LocationOnOutlined, ManageAccountsOutlined, WorkOutlineOutlined } from '@mui/icons-material'
 
 export default function UserWidget() {
   /* Theme configuration */
   const { palette } = useTheme() as any
   const dark = palette.neutral.dark
   const medium = palette.neutral.medium
+  const main = palette.neutral.main
 
   return (
     <WidgetWrapper>
-
       {/* First row */}
       <FlexBetween gap='.5rem' pb='1.1rem'>
         <FlexBetween gap='1rem'>
@@ -40,6 +40,16 @@ export default function UserWidget() {
       <Divider />
 
       {/* Second row */}
+      <Box p='1rem 0'>
+        <Box display='flex' alignItems='center' gap='1rem' mb='.5rem'>
+          <LocationOnOutlined fontSize='large' sx={{ color: main }} />
+          <Typography color={medium}>Antananarivo Madagascar</Typography>
+        </Box>
+        <Box display='flex' alignItems='center' gap='1rem' mb='.5rem'>
+          <WorkOutlineOutlined fontSize='large' sx={{ color: main }} />
+          <Typography color={medium}>Je ne travaille pas je suis une princesse</Typography>
+        </Box>
+      </Box>
     </WidgetWrapper>
   )
 }
